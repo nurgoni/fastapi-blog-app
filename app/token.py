c89df84bedeb13774cc5241bb248a6e722628472
AnnurAfgoni <afgoniannur@gmail.com>
2025-02-05 06:39:57 +0800

    edit token module

--- app/token.py
+++ app/token.py
@@ -3,9 +3,9 @@ from typing import Optional
 from jose import jwt, JWTError
 from app import schemas
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=".env")
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
