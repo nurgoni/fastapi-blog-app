52b521a4dfb3c4e1181e95b7609afcf0172c08f5
AnnurAfgoni <afgoniannur@gmail.com>
2025-02-05 00:30:49 +0800

    feat: add configuration module

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,5 +1,24 @@
-class Settings:
-    PROJECT_NAME: str = "FastAPI Blog"
-    PROJECT_VERSION: str = "1.0.0"
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+    PROJECT_VERSION: str
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: str
+    POSTGRES_DB: str
+
+    @property
+    def DATABASE_URL(self):
+        return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
+    class Config(SettingsConfigDict):
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        extra = "ignore"
+
 
 settings = Settings()
